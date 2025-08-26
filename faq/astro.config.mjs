348b317818d398338478b964272e3049707c9948
vinividivicilala <aji862391@gmail.com>
2025-08-26 12:52:19 +0700

    Update astro.config.mjs

--- faq/astro.config.mjs
+++ faq/astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   build: {
-    outDir: '../public/faq',   // hasil Astro ke public/faq
+    outDir: '../public/faq',   // build ke repo/public/faq
   },
-  site: 'https://noted-farid.netlify.app',
 });
