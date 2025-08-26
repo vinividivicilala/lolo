d5019c0a0f6d7ff082d7205c4481ac655839bce3
vinividivicilala <aji862391@gmail.com>
2025-08-26 13:16:22 +0700

    Update astro.config.mjs

--- faq/astro.config.mjs
+++ faq/astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
+// faq/astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
   build: {
-    outDir: '../public/faq',   // build ke repo/public/faq
+    outDir: '../public/faq',   // hasil build keluar ke root/public/faq
   },
 });
