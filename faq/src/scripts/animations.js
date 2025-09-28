f445bc65226a47345a4a980022e2ddb0ba87ea20
vinividivicilala <aji862391@gmail.com>
2025-09-28 15:23:20 +0700

    Update animations.js

--- faq/src/scripts/animations.js
+++ faq/src/scripts/animations.js
@@ -2,35 +2,37 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
 
-gsap.registerPlugin(ScrollTrigger);
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
 
-// Lenis Smooth Scroll
-const lenis = new Lenis();
-function raf(time) {
-  lenis.raf(time);
+  // Lenis Smooth Scroll
+  const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
   requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
 
-// Animasi header
-gsap.from(".big-title", {
-  y: -50,
-  opacity: 0,
-  duration: 1.2,
-  ease: "power3.out",
-});
-
-// Animasi tiap card
-gsap.utils.toArray(".linebox").forEach((card) => {
-  gsap.from(card, {
-    scrollTrigger: {
-      trigger: card,
-      start: "top 80%",
-      toggleActions: "play none none reverse",
-    },
-    y: 50,
+  // Animasi header
+  gsap.from(".big-title", {
+    y: -50,
     opacity: 0,
-    duration: 0.8,
-    ease: "power2.out",
+    duration: 1.2,
+    ease: "power3.out",
   });
-});
+
+  // Animasi tiap card
+  gsap.utils.toArray(".linebox").forEach((card) => {
+    gsap.from(card, {
+      scrollTrigger: {
+        trigger: card,
+        start: "top 80%",
+        toggleActions: "play none none reverse",
+      },
+      y: 50,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+    });
+  });
+}
