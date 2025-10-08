67e9492c8e068ee25d858bcea5f06aaaed2c27bc
vinividivicilala <aji862391@gmail.com>
2025-10-08 14:25:16 +0700

    Delete faq/astro.config.mjs

--- faq/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  out: 'static',  // Pastikan 'static' dalam tanda kutip
-  integrations: [react()],
-});
