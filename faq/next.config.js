1bceeb1226a00cbae0adfcb13636824b820966a8
vinividivicilala <aji862391@gmail.com>
2025-10-09 12:17:50 +0700

    Update next.config.js

--- faq/next.config.js
+++ faq/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,  // Enable the experimental appDir feature
-  },
   images: {
     domains: ['localhost'],
   },
