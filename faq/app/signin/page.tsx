7132ef19f7a3382512f9d3a73e195ea89cce86de
vinividivicilala <aji862391@gmail.com>
2025-10-20 17:45:37 +0700

    Update page.tsx

--- faq/app/signin/page.tsx
+++ faq/app/signin/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
+
 export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgotPassword }: SignInPageProps) {
   const router = useRouter();
 
@@ -33,7 +34,7 @@ export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgot
           overflow: 'hidden'
         }}
       >
-        {/* Portrait Image - Full Size Background */}
+        {/* Portrait Image - Half Page Size */}
         <div
           style={{
             width: '100%',
@@ -45,51 +46,6 @@ export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgot
             borderRadius: '20px'
           }}
         />
-        
-        {/* Black Overlay on Right Side with Text */}
-        <div
-          style={{
-            position: 'absolute',
-            right: '0',
-            top: '0',
-            width: '40%', // Sesuaikan lebar sesuai porsi yang sudah ada
-            height: '100%',
-            backgroundColor: '#000',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            padding: '0 3rem',
-            borderRadius: '0 20px 20px 0'
-          }}
-        >
-          {/* Judul Besar + Tebal */}
-          <h1
-            style={{
-              fontFamily: 'Arial, sans-serif',
-              fontSize: '3.5rem',
-              fontWeight: 'bold',
-              color: '#fff',
-              marginBottom: '1rem',
-              lineHeight: '1.2'
-            }}
-          >
-            Welcome back
-          </h1>
-          
-          {/* Deskripsi */}
-          <p
-            style={{
-              fontFamily: 'Arial, sans-serif',
-              fontSize: '1.2rem',
-              color: '#ccc',
-              margin: 0,
-              lineHeight: '1.5'
-            }}
-          >
-            Sign in to your account to continue
-          </p>
-        </div>
       </div>
     </div>
   );
