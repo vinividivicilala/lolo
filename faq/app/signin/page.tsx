13625f5c1e12733bfd78e4e91e289dc65b028058
vinividivicilala <aji862391@gmail.com>
2025-10-20 19:54:47 +0700

    Update page.tsx

--- faq/app/signin/page.tsx
+++ faq/app/signin/page.tsx
@@ -25,8 +25,7 @@ export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgot
       <div
         style={{
           width: '1300px',
-          height: '1000px',
-          backgroundColor: 'rgba(255, 255, 255, 0.1)', // Putih transparan tipis
+          height: '800px',
           borderRadius: '20px',
           border: '1px solid rgba(255, 255, 255, 0.3)', // Border putih transparan
           boxShadow: '0 4px 15px rgba(0,0,0,0.1)', // Shadow lebih soft
@@ -36,3 +35,4 @@ export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgot
     </div>
   );
 }
+
