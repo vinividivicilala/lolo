f781cb8ff3f6a1bad83b734641df9bb49f6101bb
vinividivicilala <aji862391@gmail.com>
2025-10-20 12:50:40 +0700

    Update page.tsx

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -6,9 +6,9 @@ import { useRouter } from "next/navigation";
 import { gsap } from "gsap";
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc, onSnapshot, collection, getDocs } from "firebase/firestore";
-import SignInPage from './components/auth/signin-page';
-import SignUpPage from './components/auth/signup-page';
-import ForgotPasswordPage from './components/auth/forgot-password-page';
+import SignInPage from '../../components/auth/signin-page';
+import SignUpPage from '../../components/auth/signup-page';
+import ForgotPasswordPage from '../../components/auth/forgot-password-page';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -2956,5 +2956,6 @@ onClick={handleOpenSignIn}  // Ganti console.log dengan ini
     </div>
   );
 }
+
 
 
