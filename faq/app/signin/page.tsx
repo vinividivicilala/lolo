b18dc7e2a97a72627933e8d1d89174006c2a472b
vinividivicilala <aji862391@gmail.com>
2025-10-20 17:24:12 +0700

    Update page.tsx

--- faq/app/signin/page.tsx
+++ faq/app/signin/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
-
 export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgotPassword }: SignInPageProps) {
   const router = useRouter();
 
@@ -46,9 +45,60 @@ export default function SignInPage({ onClose, onSwitchToSignUp, onSwitchToForgot
             borderRadius: '20px'
           }}
         />
+
+        {/* Content Overlay - Right Side */}
+        <div
+          style={{
+            position: 'absolute',
+            right: '4rem',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            textAlign: 'right',
+            color: 'white',
+            maxWidth: '400px'
+          }}
+        >
+          {/* Judul Besar - Arame */}
+          <h1
+            style={{
+              fontFamily: 'Roboto',
+              fontSize: '4rem',
+              fontWeight: 'bold',
+              margin: '0 0 2rem 0',
+              lineHeight: '1.1',
+              textShadow: '2px 2px 4px rgba(0,0,0,0.5)'
+            }}
+          >
+            ARAME
+          </h1>
+          
+          {/* Welcome Back Title */}
+          <h2
+            style={{
+              fontFamily: 'Roboto',
+              fontSize: '2.5rem',
+              fontWeight: '600',
+              margin: '0 0 1rem 0',
+              textShadow: '2px 2px 4px rgba(0,0,0,0.5)'
+            }}
+          >
+            Welcome Back
+          </h2>
+          
+          {/* Description */}
+          <p
+            style={{
+              fontFamily: 'Roboto',
+              fontSize: '1.1rem',
+              margin: '0',
+              lineHeight: '1.5',
+              textShadow: '1px 1px 2px rgba(0,0,0,0.5)'
+            }}
+          >
+            Sign in to your account to continue
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
-
