31654ef6f53f0bd476210f6ae2a368a709cc4967
vinividivicilala <aji862391@gmail.com>
2026-02-04 09:52:13 +0700

    Update page.tsx

--- faq/app/update/page.tsx
+++ faq/app/update/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
 
 export default function UpdatePage() {
-  const router = useRouter();
-
   return (
     <div style={{
       minHeight: '100vh',
@@ -14,15 +11,12 @@ export default function UpdatePage() {
       padding: '2rem',
       fontFamily: 'Helvetica, Arial, sans-serif'
     }}>
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '2rem' }}>
-        <h1 style={{ fontSize: '3rem', marginRight: '1rem' }}>Website Title</h1>
-        <span style={{ 
-          fontSize: '3rem',
-          color: 'rgba(255,255,255,0.8)'
-        }}>
-          Menuru
-        </span>
-      </div>
+      <h1 style={{
+        fontSize: '3rem',
+        color: 'white'
+      }}>
+        Menuru
+      </h1>
     </div>
   );
 }
