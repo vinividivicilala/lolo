4bd967c82db1b72ecdc0a234ce00a150a40d6a92
vinividivicilala <aji862391@gmail.com>
2026-04-09 20:28:30 +0700

    Update page.tsx

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -4,61 +4,69 @@ import React from "react";
 
 export default function HomePage(): React.JSX.Element {
   return (
-    <div style={{
-      minHeight: '100vh',
-      backgroundColor: 'black',
-      margin: 0,
-      padding: 0,
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      WebkitFontSmoothing: 'antialiased',
-      MozOsxFontSmoothing: 'grayscale'
-    }}>
-      {/* Framed Layout */}
-      <div style={{
-        position: 'fixed',
-        top: '2rem',
-        left: '2rem',
-        right: '2rem',
-        bottom: '2rem',
-        backgroundColor: '#dbd6c9',
-        borderRadius: '20px',
-        zIndex: 1,
-        pointerEvents: 'none'
-      }} />
+    <>
+      {/* Load Inter Tight font dari Google Fonts */}
+      <link 
+        href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400&display=swap" 
+        rel="stylesheet"
+      />
       
-      {/* Teks MENURU */}
       <div style={{
-        position: 'fixed',
-        top: 'calc(2rem + 16px)',
-        left: 'calc(2rem + 20px)',
-        zIndex: 2,
-        pointerEvents: 'none'
-      }}>
-        <span style={{
-          fontFamily: 'a2g, monospace, sans-serif',
-          fontWeight: 400,
-          color: 'rgb(0, 20, 70)',
-          fontSize: '13px',
-          lineHeight: '13px',
-          fontStyle: 'normal'
-        }}>
-          MENURU
-        </span>
-      </div>
-      
-      {/* Area konten */}
-      <div style={{
-        position: 'relative',
-        zIndex: 2,
+        minHeight: '100vh',
+        backgroundColor: 'black',
+        margin: 0,
+        padding: 0,
         width: '100%',
-        height: '100vh',
-        padding: '2rem',
-        boxSizing: 'border-box'
-      }} />
-    </div>
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        WebkitFontSmoothing: 'antialiased',
+        MozOsxFontSmoothing: 'grayscale'
+      }}>
+        {/* Framed Layout */}
+        <div style={{
+          position: 'fixed',
+          top: '2rem',
+          left: '2rem',
+          right: '2rem',
+          bottom: '2rem',
+          backgroundColor: '#dbd6c9',
+          borderRadius: '20px',
+          zIndex: 1,
+          pointerEvents: 'none'
+        }} />
+        
+        {/* Teks MENURU */}
+        <div style={{
+          position: 'fixed',
+          top: 'calc(2rem + 16px)',
+          left: 'calc(2rem + 20px)',
+          zIndex: 2,
+          pointerEvents: 'none'
+        }}>
+          <span style={{
+            fontFamily: "'Inter Tight', sans-serif",
+            fontWeight: 400,
+            color: 'rgb(0, 20, 70)',
+            fontSize: '13px',
+            lineHeight: '13px',
+            fontStyle: 'normal'
+          }}>
+            MENURU
+          </span>
+        </div>
+        
+        {/* Area konten */}
+        <div style={{
+          position: 'relative',
+          zIndex: 2,
+          width: '100%',
+          height: '100vh',
+          padding: '2rem',
+          boxSizing: 'border-box'
+        }} />
+      </div>
+    </>
   );
 }
