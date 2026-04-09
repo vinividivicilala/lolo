2b62aa4f22fb3b4701cce72c801b7871949f0431
vinividivicilala <aji862391@gmail.com>
2026-04-09 20:48:04 +0700

    Update page.tsx

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -4,67 +4,61 @@ import React from "react";
 
 export default function HomePage(): React.JSX.Element {
   return (
-    <>
-      {/* Load font premium-style gratis dari Fontshare (seperti desain Awwwards) */}
-      <link href="https://api.fontshare.com/v2/css?f[]=general-sans@400&display=swap" rel="stylesheet" />
-      
+    <div style={{
+      minHeight: '100vh',
+      backgroundColor: 'black',
+      margin: 0,
+      padding: 0,
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      WebkitFontSmoothing: 'antialiased',
+      MozOsxFontSmoothing: 'grayscale'
+    }}>
+      {/* Framed Layout */}
       <div style={{
-        minHeight: '100vh',
-        backgroundColor: 'black',
-        margin: 0,
-        padding: 0,
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        WebkitFontSmoothing: 'antialiased',
-        MozOsxFontSmoothing: 'grayscale'
+        position: 'fixed',
+        top: '2rem',
+        left: '2rem',
+        right: '2rem',
+        bottom: '2rem',
+        backgroundColor: '#dbd6c9',
+        borderRadius: '20px',
+        zIndex: 1,
+        pointerEvents: 'none'
+      }} />
+      
+      {/* Teks MENURU */}
+      <div style={{
+        position: 'fixed',
+        top: 'calc(2rem + 16px)',
+        left: 'calc(2rem + 20px)',
+        zIndex: 2,
+        pointerEvents: 'none'
       }}>
-        {/* Framed Layout */}
-        <div style={{
-          position: 'fixed',
-          top: '2rem',
-          left: '2rem',
-          right: '2rem',
-          bottom: '2rem',
-          backgroundColor: '#dbd6c9',
-          borderRadius: '20px',
-          zIndex: 1,
-          pointerEvents: 'none'
-        }} />
-        
-        {/* Teks MENURU */}
-        <div style={{
-          position: 'fixed',
-          top: 'calc(2rem + 16px)',
-          left: 'calc(2rem + 20px)',
-          zIndex: 2,
-          pointerEvents: 'none'
+        <span style={{
+          fontFamily: 'Helvetica, sans-serif',
+          fontWeight: 400,
+          fontStyle: 'normal',
+          color: 'rgb(0, 20, 70)',
+          fontSize: '13px',
+          lineHeight: '13px'
         }}>
-          <span style={{
-            fontFamily: "'General Sans', sans-serif",
-            fontWeight: 400,
-            fontStyle: 'normal',
-            color: 'rgb(0, 20, 70)',
-            fontSize: '13px',
-            lineHeight: '13px',
-            letterSpacing: '0.5px'
-          }}>
-            MENURU
-          </span>
-        </div>
-        
-        {/* Area konten */}
-        <div style={{
-          position: 'relative',
-          zIndex: 2,
-          width: '100%',
-          height: '100vh',
-          padding: '2rem',
-          boxSizing: 'border-box'
-        }} />
+          MENURU
+        </span>
       </div>
-    </>
+      
+      {/* Area konten */}
+      <div style={{
+        position: 'relative',
+        zIndex: 2,
+        width: '100%',
+        height: '100vh',
+        padding: '2rem',
+        boxSizing: 'border-box'
+      }} />
+    </div>
   );
 }
