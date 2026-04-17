799546646453cdc6c686ce32c2789820b86d7819
vinividivicilala <aji862391@gmail.com>
2026-04-17 14:52:14 +0700

    Update page.tsx

--- faq/app/contact/page.tsx
+++ faq/app/contact/page.tsx
@@ -601,47 +601,42 @@ export default function ContactPage(): React.JSX.Element {
                 You can know contact Website this Menuru
               </div>
 
-           {/* Baris 01 dan Note */}
-<div style={{
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  gap: '60px',
-  position: 'relative',
-  paddingLeft: '80px'
-}}>
-  {/* 01 - kecil & ringan */}
-  <div
-    style={{
-      fontFamily: "'Inter', 'Helvetica Neue', Arial, sans-serif",
-      fontSize: '32px',
-      fontWeight: '400',
-      color: '#111',
-      letterSpacing: '2px',
-      lineHeight: '1'
-    }}
-    onMouseEnter={() => setShowNoteText(true)}
-    onMouseLeave={() => setShowNoteText(false)}
-  >
-    01
-  </div>
+              {/* Baris 01 di kiri dan Note di sebelahnya dengan jarak agak dekat */}
+              <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '60px'
+              }}>
+                {/* 01 - Sisi Kiri - font minimalis 24px */}
+                <div
+                  style={{
+                    fontFamily: "'Questrial', sans-serif",
+                    fontSize: '24px',
+                    fontWeight: '400',
+                    color: '#000000',
+                    cursor: 'pointer'
+                  }}
+                  onMouseEnter={() => setShowNoteText(true)}
+                  onMouseLeave={() => setShowNoteText(false)}
+                >
+                  01
+                </div>
 
-  {/* Note jadi besar & clean */}
-  <div
-    style={{
-      fontFamily: "'Inter', 'Helvetica Neue', Arial, sans-serif",
-      fontSize: '160px',
-      fontWeight: '400',
-      color: '#111',
-      letterSpacing: '-2px',
-      lineHeight: '1'
-    }}
-    onMouseEnter={() => setShowNoteText(true)}
-    onMouseLeave={() => setShowNoteText(false)}
-  >
-    Note
-  </div>
-</div>
+                {/* Note - font minimalis 24px */}
+                <div
+                  style={{
+                    fontFamily: "'Questrial', sans-serif",
+                    fontSize: '24px',
+                    fontWeight: '400',
+                    color: '#000000',
+                    cursor: 'pointer'
+                  }}
+                  onMouseEnter={() => setShowNoteText(true)}
+                  onMouseLeave={() => setShowNoteText(false)}
+                >
+                  Note
+                </div>
+              </div>
 
               {/* Teks hover yang muncul di samping kanan Note - dengan GSAP SplitText */}
               {showNoteText && (
@@ -649,7 +644,7 @@ export default function ContactPage(): React.JSX.Element {
                   ref={hoverTextRef}
                   style={{
                     fontFamily: "'Questrial', sans-serif",
-                    fontSize: '24px',
+                    fontSize: '20px',
                     fontWeight: '400',
                     color: '#000000',
                     position: 'absolute',
