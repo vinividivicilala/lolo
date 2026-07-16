4162f489496f9584b1820df1a4c8725e209f7a99
vinividivicilala <aji862391@gmail.com>
2026-07-16 07:57:00 +0700

    Update next.config.js

--- faq/next.config.js
+++ faq/next.config.js
@@ -1,10 +1,4 @@
 const path = require("path");
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -37,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig; // ✅ Tanpa withPWA
