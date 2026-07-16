5fc748a9ab5fd5272574136fbd635647b018cee4
vinividivicilala <aji862391@gmail.com>
2026-07-16 09:56:00 +0700

    Update next.config.js

--- faq/next.config.js
+++ faq/next.config.js
@@ -1,20 +1,6 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-      allowedOrigins: ["*"],
-    },
-  },
-
-  images: {
-    unoptimized: true,
-    domains: ["localhost"],
-  },
 
   eslint: {
     ignoreDuringBuilds: true,
@@ -22,16 +8,6 @@ const nextConfig = {
 
   typescript: {
     ignoreBuildErrors: true,
-  },
-
-  // ❌ HAPUS outputFileTracingIncludes
-  // outputFileTracingIncludes: {
-  //   '/': ['./app/**/*'],
-  // },
-
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
   },
 };
 
