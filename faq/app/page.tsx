e20c57a65cf129b6584e42165c26d67142f2aed3
vinividivicilala <aji862391@gmail.com>
2026-09-01 19:46:45 +0700

    Update page.tsx

--- faq/app/page.tsx
+++ faq/app/page.tsx
@@ -2772,14 +2772,14 @@ export default function HomePage(): React.JSX.Element {
                 backgroundColor: "rgba(13, 60, 252, 0.1)",
                 borderRadius: "6px",
                 padding: "4px",
-                width: "70px",
-                height: "70px",
+                width: "100px",
+                height: "100px",
                 overflow: "hidden",
                 flexShrink: 0,
               }}
             >
               <img
-                src="/images/10.jpg"
+                src="/images/mnbv.JPG"
                 alt="Menuru"
                 style={{
                   width: "100%",
